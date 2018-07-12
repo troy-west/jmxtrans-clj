8815f43db5d6d6420bf53dd5e5ad3953a2a1d896
Derek Troy-West <derek@troywest.com>
2018-07-12 12:55:31 +1000

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.troy-west/jmxtrans-clj "0.1.2"
+(defproject com.troy-west/jmxtrans-clj "0.1.3-SNAPSHOT"
   :description "Clojure wrapper around jmxtrans"
 
   :url "https://github.com/troy-west/jmxtrans-clj"
